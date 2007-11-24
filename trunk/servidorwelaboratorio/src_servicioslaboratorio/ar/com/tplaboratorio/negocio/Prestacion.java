797d8f201c033778b806694c8a9f9b8558055dc2
pamtru <pamtru@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-24 19:48:08 +0000

    Subi el modelo de negocio del laboratorio

---
+++ trunk/servidorwelaboratorio/src_servicioslaboratorio/ar/com/tplaboratorio/negocio/Prestacion.java
@@ -0,0 +1,15 @@
+package ar.com.tplaboratorio.negocio;
+
+public class Prestacion {
+	private String descripcion;
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+}
