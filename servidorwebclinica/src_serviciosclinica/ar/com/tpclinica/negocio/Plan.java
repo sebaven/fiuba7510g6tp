1944e58d8b100fcb4d2b793b20ccd0db8dab20f6
pamtru <pamtru@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-24 20:19:17 +0000

    implementacion metodo getPosiblesItemsOrdenMedicas()

--- servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/Plan.java
+++ servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/Plan.java
@@ -1,8 +1,11 @@
 package ar.com.tpclinica.negocio;
 
+import ar.com.tpclinica.negocio.reglas.Comparar;
+
 public class Plan {
 	
 	public boolean validarOrden(OrdenMedica ordenMedica) {
+				
 		return true;
 	}
 
