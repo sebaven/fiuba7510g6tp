9e875a30c2aee9541598755574dafccd5edc55e0
pamtru <pamtru@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 12:22:55 +0000

    codeado validarOrden.

--- trunk/servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/Plan.java
+++ trunk/servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/Plan.java
@@ -1,11 +1,27 @@
 package ar.com.tpclinica.negocio;
 
+import java.util.Iterator;
+import java.util.List;
+
 import ar.com.tpclinica.negocio.exepciones.OrdenMedicaInvalidaException;
+import ar.com.tpclinica.negocio.reglas.Operacion;
 
 public class Plan {
-
+	private List<Operacion> operaciones;
+	
 	public void validarOrden(OrdenMedica ordenMedica) throws OrdenMedicaInvalidaException{
-
+		Iterator<Operacion> it=operaciones.iterator();
+		
+		while (it.hasNext()){
+			it.next().aplicar(ordenMedica);
+			
+		}
 	}
-
+	public void agregarOperacion(Operacion o){
+		operaciones.add(o);
+	}
+	public void borrarOperacion(Operacion o){
+		operaciones.remove(o);
+		
+	}
 }
