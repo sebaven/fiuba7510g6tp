c4cc16223764389c2251225718e125e3f019d22f
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 04:56:22 +0000

    Ahora OrdenMedica tiene ID

--- servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/OrdenMedica.java
+++ servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/negocio/OrdenMedica.java
@@ -3,90 +3,131 @@ package ar.com.tpclinica.negocio;
 import java.util.List;
 
 public class OrdenMedica {
-	private OrdenMedicaEstado estado;
-	private boolean enviadaALab;
-	private String diagnostico;
-	private Paciente paciente;
-	private Medico medico;
-	private List<OrdenMedicaItem> items;
+
+	private int id;
 	
+	private OrdenMedicaEstado estado;
+
+	private boolean enviadaALab;
+
+	private String diagnostico;
+
+	private Paciente paciente;
+
+	private Medico medico;
+
+	private List<OrdenMedicaItem> items;
+
 	public void cambiarEstado(OrdenMedicaEstado nuevoEstado) {
 		estado = nuevoEstado;
 	}
+
 	public void agregarItem(OrdenMedicaItem item) {
 		items.add(item);
 	}
+
 	public void setDiagnostico(String diag) {
 		diagnostico = diag;
 	}
+
 	public int getIdPaciente() {
 		return paciente.id;
 	}
+
 	/**
 	 * @return the enviadaALab
 	 */
 	public boolean isEnviadaALab() {
 		return enviadaALab;
 	}
+
 	/**
-	 * @param enviadaALab the enviadaALab to set
+	 * @param enviadaALab
+	 *            the enviadaALab to set
 	 */
 	public void setEnviadaALab(boolean enviadaALab) {
 		this.enviadaALab = enviadaALab;
 	}
+
 	/**
 	 * @return the estado
 	 */
 	public OrdenMedicaEstado getEstado() {
 		return estado;
 	}
+
 	/**
-	 * @param estado the estado to set
+	 * @param estado
+	 *            the estado to set
 	 */
 	public void setEstado(OrdenMedicaEstado estado) {
 		this.estado = estado;
 	}
+
 	/**
 	 * @return the items
 	 */
 	public List<OrdenMedicaItem> getItems() {
 		return items;
 	}
+
 	/**
-	 * @param items the items to set
+	 * @param items
+	 *            the items to set
 	 */
 	public void setItems(List<OrdenMedicaItem> items) {
 		this.items = items;
 	}
+
 	/**
 	 * @return the medico
 	 */
 	public Medico getMedico() {
 		return medico;
 	}
+
 	/**
-	 * @param medico the medico to set
+	 * @param medico
+	 *            the medico to set
 	 */
 	public void setMedico(Medico medico) {
 		this.medico = medico;
 	}
+
 	/**
 	 * @return the paciente
 	 */
 	public Paciente getPaciente() {
 		return paciente;
 	}
+
 	/**
-	 * @param paciente the paciente to set
+	 * @param paciente
+	 *            the paciente to set
 	 */
 	public void setPaciente(Paciente paciente) {
 		this.paciente = paciente;
 	}
+
 	/**
 	 * @return the diagnostico
 	 */
 	public String getDiagnostico() {
 		return diagnostico;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
