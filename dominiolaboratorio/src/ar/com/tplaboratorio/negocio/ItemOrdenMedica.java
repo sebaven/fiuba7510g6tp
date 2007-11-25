1eec60ef1fb29b05ae7234243a6c79c73fb07bb8
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 22:23:05 +0000

    cambio en la estructura del proyecto

---
+++ dominiolaboratorio/src/ar/com/tplaboratorio/negocio/ItemOrdenMedica.java
@@ -0,0 +1,32 @@
+package ar.com.tplaboratorio.negocio;
+
+public class ItemOrdenMedica {
+	private String descripcion;
+	private Resultado resultado;
+	private Prestacion prestacion;
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Resultado getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Resultado resultado) {
+		this.resultado = resultado;
+	}
+
+	public Prestacion getPrestacion() {
+		return prestacion;
+	}
+
+	public void setPrestacion(Prestacion prestacion) {
+		this.prestacion = prestacion;
+	}
+
+}
---
+++ dominiolaboratorio/src/ar/com/tplaboratorio/negocio/Prestacion.java
@@ -0,0 +1,33 @@
+package ar.com.tplaboratorio.negocio;
+
+public class Prestacion {
+	private String descripcion;
+	private int id;
+	private int codigo;
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+}
