ee8dcfa5dc1142812e8e36be926a6bb61cf1b1a1
jmbriano <jmbriano@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 16:00:59 +0000

    implementacion del hidratarOrdenes

--- servidorwebclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/Hidratador.java
+++ servidorwebclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/Hidratador.java
@@ -80,7 +80,104 @@ public class Hidratador {
 	}
 	
 	public void hidratarOrdenes(Repositorio<OrdenMedica> repo){
+		int cantidad = 4;
+		OrdenMedica om[] = new OrdenMedica[cantidad];
 		
+		Repositorio<Prestacion> repPrestaciones = RepositoriosProvider.getInstancia().getRepositorioPrestacion();
+		RepositorioMedicos repMedicos = RepositoriosProvider.getInstancia().getRepositorioMedico();
+		Repositorio<Paciente> repPacientes = RepositoriosProvider.getInstancia().getRepositorioPacientes();
+		
+		for (int i=0; i<cantidad; i++){
+			om[i] = new OrdenMedica();
+		}
+		
+		//Orden Medica 0
+		om[0].setId(800001);  
+		om[0].cambiarEstado(new OrdenMedicaEstado(OrdenMedicaEstado.PENDIENTE_AUTORIZACION));
+		om[0].setMedico(repMedicos.get(5000)); //Mariano sanchez, Pediatria
+		om[0].setPaciente(repPacientes.get(1130));
+		om[0].setDiagnostico("Paciente con dificultades para respirar al realizar ejercicios repentinos");
+		om[0].setEnviadaALab(false);
+		
+		//items
+		OrdenMedicaItem it1 = new OrdenMedicaItem();
+		it1.setPrestacion(repPrestaciones.get(0)); //radiografia
+		it1.setDescripcion("Rx de torax en 50x60");
+		om[0].agregarItem(it1);
+		
+		OrdenMedicaItem it2 = new OrdenMedicaItem();
+		it2.setPrestacion(repPrestaciones.get(4)); //Electrocaridograma
+		it2.setDescripcion("Con ejercicio fisico previo");
+		om[0].agregarItem(it2);
+		
+		
+		//Orden Medica 1
+		om[1].setId(800002); 
+		om[1].cambiarEstado(new OrdenMedicaEstado(OrdenMedicaEstado.AUTORIZADA));
+		om[1].setMedico(repMedicos.get(5002)); //Emiliano Perez, oftalmologia
+		om[1].setPaciente(repPacientes.get(1251));
+		om[1].setDiagnostico("Revisacion general");
+		om[1].setEnviadaALab(false);
+		
+		//items
+		OrdenMedicaItem it3 = new OrdenMedicaItem();
+		it3.setPrestacion(repPrestaciones.get(0)); //radiografia
+		it3.setDescripcion("Rx de Craneo");
+		om[1].agregarItem(it3);
+
+		
+		//Orden Medica 2
+		om[2].setId(800003); 
+		om[2].cambiarEstado(new OrdenMedicaEstado(OrdenMedicaEstado.AUTORIZADA));
+		om[2].setMedico(repMedicos.get(5001)); //Laura Lopez, dermatologa
+		om[2].setPaciente(repPacientes.get(1255));
+		om[2].setDiagnostico("Consulta por manchas solares");
+		om[2].setEnviadaALab(true);
+		
+		//items
+		OrdenMedicaItem it4 = new OrdenMedicaItem();
+		it4.setPrestacion(repPrestaciones.get(1)); //analisis de sangre
+		it4.setDescripcion("Plaquetas, globulos rojos, globulos blancos, glucemia");
+		om[2].agregarItem(it4);
+		
+		OrdenMedicaItem it5 = new OrdenMedicaItem();
+		it5.setPrestacion(repPrestaciones.get(2)); //analisis de orina
+		it5.setDescripcion("Glucemia");
+		om[2].agregarItem(it5);
+		
+		OrdenMedicaItem it6 = new OrdenMedicaItem();
+		it6.setPrestacion(repPrestaciones.get(1)); //tomografia
+		it6.setDescripcion("Computada de craneo");
+		om[2].agregarItem(it6);
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		Repositorio<Especialidad> repEsp = RepositoriosProvider.getInstancia().getRepositorioEspecialidad();
+		
+		Medico m[] = new Medico[cantidad];
+		
+		for (int i=0; i<cantidad; i++){
+			m[i] = new Medico();
+		}
+		
+		m[0].setNombre("Mariano"); m[0].setApellido("Sanchez");
+		m[0].setId(5000); m[0].setEspecialidad(repEsp.get(0));
+		
+		m[1].setNombre("Emiliano"); m[1].setApellido("Perez");
+		m[1].setId(5001); m[1].setEspecialidad(repEsp.get(1));
+		
+		m[2].setNombre("Laura"); m[2].setApellido("Lopez");
+		m[2].setId(5002); m[2].setEspecialidad(repEsp.get(2));		
+		
+		for (int i=0; i<cantidad; i++){
+			//repo.modify(m[i].getId(), m[i]);
+		}
 	}
 	
 	public void hidratarPacientes(Repositorio<Paciente> repo){
