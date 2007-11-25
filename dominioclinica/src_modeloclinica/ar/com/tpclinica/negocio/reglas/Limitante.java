57706e5266a3c80b5f87fd6b41377b7b35d8d2a7
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 21:52:01 +0000

    cambio en la estructura del proyecto

---
+++ dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/reglas/Limitante.java
@@ -0,0 +1,59 @@
+package ar.com.tpclinica.negocio.reglas;
+
+public class Limitante {
+	private String nombre, buscarEn, valor;
+	private int periodo;
+	/**
+	 * @return the buscarEn
+	 */
+	public String getBuscarEn() {
+		return buscarEn;
+	}
+	/**
+	 * @param buscarEn the buscarEn to set
+	 */
+	public void setBuscarEn(String buscarEn) {
+		this.buscarEn = buscarEn;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the periodo
+	 */
+	public int getPeriodo() {
+		return periodo;
+	}
+	/**
+	 * @param periodo the periodo to set
+	 */
+	public void setPeriodo(int periodo) {
+		this.periodo = periodo;
+	}
+	/**
+	 * @return the valor
+	 */
+	public String getValor() {
+		return valor;
+	}
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+}
+
+
+
+
