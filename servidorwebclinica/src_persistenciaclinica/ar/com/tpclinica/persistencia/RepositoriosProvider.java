8088e63c41cea1f5bf0c729890e0358f499722ce
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 03:25:58 +0000

    cambiamos a la interface y no la implementacion

--- servidorwebclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositoriosProvider.java
+++ servidorwebclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositoriosProvider.java
@@ -48,7 +48,7 @@ public class RepositoriosProvider {
 		return repoMedicos;
 	}
 	
-	public RepositorioOrdenesImp getRepositorioOrdenes(){
+	public RepositorioOrdenes getRepositorioOrdenes(){
 		return repoOrdenes;
 	}
 	
