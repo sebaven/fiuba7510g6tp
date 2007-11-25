6a516c9edcfdc944d705f213d42240309ef817cc
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 05:04:10 +0000

    Implementacion de metodos

--- servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/serviciosaplicacion/ServiciosAplicacionClinicaOrdenMedica.java
+++ servidorwebclinica/src_serviciosclinica/ar/com/tpclinica/serviciosaplicacion/ServiciosAplicacionClinicaOrdenMedica.java
@@ -1,41 +1,69 @@
 package ar.com.tpclinica.serviciosaplicacion;
 
 import ar.com.tpclinica.negocio.OrdenMedica;
+import ar.com.tpclinica.negocio.OrdenMedicaEstado;
 import ar.com.tpclinica.negocio.OrdenMedicaItem;
+import ar.com.tpclinica.negocio.Plan;
+import ar.com.tpclinica.negocio.exepciones.OrdenMedicaInvalidaException;
 import ar.com.tpclinica.persistencia.Repositorio;
+import ar.com.tpclinica.persistencia.RepositorioOrdenes;
+import ar.com.tpclinica.persistencia.RepositoriosProvider;
 
 public class ServiciosAplicacionClinicaOrdenMedica extends
 		ServiciosAplicacionClinica {
 	private Repositorio<OrdenMedica> respositorioOrdenesMedicas;
+
 	public void autorizarOrdenMedica(int idOrdenMedica) {
-	
+		RepositorioOrdenes repositorioOrdenes = (RepositorioOrdenes) RepositoriosProvider.getInstancia().getRepositorioOrdenes();
+		OrdenMedica ordenMedica = repositorioOrdenes.get(idOrdenMedica);
+		OrdenMedicaEstado ordenMedicaEstado = new OrdenMedicaEstado();
+		ordenMedicaEstado.setDescripcion("Autorizada");
+		ordenMedica.cambiarEstado(ordenMedicaEstado);
+		repositorioOrdenes.modify(ordenMedica.getId(), ordenMedica);
+
 	}
+
 	public void cancelarOrdenMedica(int idOrdenMedica) {
-		
+
 	}
+
 	public OrdenMedica getOrdenMedica(int idOrdenMedica) {
 		return respositorioOrdenesMedicas.get(idOrdenMedica);
 	}
-	public OrdenMedica crearNuevaOrdenMedica(int idMedico, int idPaciente) {
-		return null;
+
+	public void crearNuevaOrdenMedica(OrdenMedica nuevaOrdenMedica)
+			throws OrdenMedicaInvalidaException {
+		Plan plan = nuevaOrdenMedica.getPaciente().getPlan();
+		plan.validarOrden(nuevaOrdenMedica);
+		RepositoriosProvider.getInstancia().getRepositorioOrdenes().add(
+				nuevaOrdenMedica);
+
 	}
+
 	public void asentarOrdenMedica(int idOrdenMedica) {
-		
+
 	}
-	public void agregarDiagnosticoAOrdenMedica(int idOrdenMedica, String diagnostico) {
-		
+
+	public void agregarDiagnosticoAOrdenMedica(int idOrdenMedica,
+			String diagnostico) {
+
 	}
-	public void agregarItemAOrdenMedica(int idOrdenMedica, OrdenMedicaItem ordenMedicaItem) {
+
+	public void agregarItemAOrdenMedica(int idOrdenMedica,
+			OrdenMedicaItem ordenMedicaItem) {
 		getOrdenMedica(idOrdenMedica).agregarItem(ordenMedicaItem);
 	}
+
 	/**
 	 * @return the respositorioOrdenesMedicas
 	 */
 	public Repositorio<OrdenMedica> getRespositorioOrdenesMedicas() {
 		return respositorioOrdenesMedicas;
 	}
+
 	/**
-	 * @param respositorioOrdenesMedicas the respositorioOrdenesMedicas to set
+	 * @param respositorioOrdenesMedicas
+	 *            the respositorioOrdenesMedicas to set
 	 */
 	public void setRespositorioOrdenesMedicas(
 			Repositorio<OrdenMedica> respositorioOrdenesMedicas) {
