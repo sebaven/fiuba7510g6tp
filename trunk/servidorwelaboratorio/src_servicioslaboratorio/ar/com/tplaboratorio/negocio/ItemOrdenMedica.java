6e9bde179808dead7f009a5c1c0efaabb5332b5e
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-25 20:43:56 +0000

    Implementacion de los web services

--- trunk/servidorwelaboratorio/src_servicioslaboratorio/ar/com/tplaboratorio/negocio/ItemOrdenMedica.java
+++ trunk/servidorwelaboratorio/src_servicioslaboratorio/ar/com/tplaboratorio/negocio/ItemOrdenMedica.java
@@ -4,23 +4,29 @@ public class ItemOrdenMedica {
 	private String descripcion;
 	private Resultado resultado;
 	private Prestacion prestacion;
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public Resultado getResultado() {
 		return resultado;
 	}
+
 	public void setResultado(Resultado resultado) {
 		this.resultado = resultado;
 	}
+
 	public Prestacion getPrestacion() {
 		return prestacion;
 	}
+
 	public void setPrestacion(Prestacion prestacion) {
 		this.prestacion = prestacion;
 	}
-	
+
 }
