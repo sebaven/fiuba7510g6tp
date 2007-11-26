edf38c638c0344af8002e47e6958eec8c58ace1d
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 06:54:45 +0000

    Constructores agregados

--- dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/Prestacion.java
+++ dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/Prestacion.java
@@ -2,31 +2,38 @@ package ar.com.tpclinica.negocio;
 
 public class Prestacion {
 	private String descripcion;
+
 	private int id;
-	
+
 	/**
-	 * Es la forma en que el usuario identifica univocamente al elemento, independientemente del id que se lo otorga el repo.
-	 * El id debe estar en una clase abstracta y comunicarse con la persistencia por medio de un LayeredType
+	 * Es la forma en que el usuario identifica univocamente al elemento,
+	 * independientemente del id que se lo otorga el repo. El id debe estar en
+	 * una clase abstracta y comunicarse con la persistencia por medio de un
+	 * LayeredType
 	 */
 	private String codigo;
+
 	/**
 	 * @return the descripcion
 	 */
-	
-	public Prestacion(){
-		
+
+	public Prestacion(String descripcion) {
+		this("sorga", descripcion);
 	}
-	public Prestacion (String codigo, String descripcion){
-	
-		this.codigo=codigo;
-		this.descripcion=descripcion;
+
+	public Prestacion(String codigo, String descripcion) {
+
+		this.codigo = codigo;
+		this.descripcion = descripcion;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
 
 	/**
-	 * @param descripcion the descripcion to set
+	 * @param descripcion
+	 *            the descripcion to set
 	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
@@ -47,10 +54,13 @@ public class Prestacion {
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public int compareTo(Prestacion prestacion) {
-		if ((this.descripcion.compareTo(prestacion.descripcion) == 0) && (this.id == prestacion.id) && (this.codigo.compareTo(prestacion.codigo) == 0)) return 0;
+		if ((this.descripcion.compareTo(prestacion.descripcion) == 0)
+				&& (this.id == prestacion.id)
+				&& (this.codigo.compareTo(prestacion.codigo) == 0))
+			return 0;
 		return 1;
 	}
-	
 
 }
