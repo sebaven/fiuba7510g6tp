71eaf0ee3231c7d253ee91b064b2438409837be1
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 08:00:51 +0000

    implementacion de los items

--- trunk/persistenciaclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositorioOrdenes.java
+++ trunk/persistenciaclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositorioOrdenes.java
@@ -4,10 +4,11 @@ import java.util.Collection;
 
 import ar.com.tpclinica.negocio.OrdenMedica;
 import ar.com.tpclinica.negocio.Paciente;
+import ar.com.tpclinica.persistencia.excepciones.ObjetoNoExisteExcepcion;
 
 public interface RepositorioOrdenes extends Repositorio<OrdenMedica>{
 
-	public OrdenMedica getPorNroOrden(int nroOrden);
+	public OrdenMedica getPorNroOrden(int nroOrden) throws ObjetoNoExisteExcepcion;
 	
 	public Collection <OrdenMedica> getPorPaciente(Paciente p);
 }
--- trunk/persistenciaclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositorioOrdenesImp.java
+++ trunk/persistenciaclinica/src_persistenciaclinica/ar/com/tpclinica/persistencia/RepositorioOrdenesImp.java
@@ -8,22 +8,29 @@ import org.apache.commons.collections.Predicate;
 
 import ar.com.tpclinica.negocio.OrdenMedica;
 import ar.com.tpclinica.negocio.Paciente;
+import ar.com.tpclinica.persistencia.excepciones.ObjetoNoExisteExcepcion;
 
 class RepositorioOrdenesImp extends RepositorioImp<OrdenMedica> implements RepositorioOrdenes {
 
 	/**
 	 * NO es lo mismo un id que un nroOrden, no es lo mismo un codigoPrestacion que un id.
+	 * @throws ObjetoNoExisteExcepcion 
 	 * 
 	 */
-	public OrdenMedica getPorNroOrden(final int nroOrden) {
-		
-		return (OrdenMedica) CollectionUtils.find(this.getAll(),new Predicate(){
+	public OrdenMedica getPorNroOrden(final int nroOrden) throws ObjetoNoExisteExcepcion {
+		System.out.println(this.getAll().size());
+	OrdenMedica orden=(OrdenMedica) CollectionUtils.find(this.getAll(),new Predicate(){
 
 			public boolean evaluate(Object arg0) {
+				System.out.println(((OrdenMedica)arg0).getId());
 				return ((OrdenMedica)arg0).getId()==nroOrden;
 			}
 			
 		});
+	if (orden==null){
+		throw new ObjetoNoExisteExcepcion();
+	}
+	return orden;
 	}
 
 	public Collection<OrdenMedica> getPorPaciente(Paciente p) {
