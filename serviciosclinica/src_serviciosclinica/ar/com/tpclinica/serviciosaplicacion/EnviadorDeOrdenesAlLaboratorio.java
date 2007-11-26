67c0d8817bfb481a6d6b150dd7c85e137944cebe
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 02:55:17 +0000

    First Load.

---
+++ serviciosclinica/src_serviciosclinica/ar/com/tpclinica/serviciosaplicacion/EnviadorDeOrdenesAlLaboratorio.java
@@ -0,0 +1,34 @@
+package ar.com.tpclinica.serviciosaplicacion;
+
+import java.util.Collection;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+
+import ar.com.tpclinica.negocio.OrdenMedica;
+import ar.com.tpclinica.negocio.OrdenMedicaEstado;
+import ar.com.tpclinica.persistencia.RepositoriosProvider;
+import ar.com.tpclinica.serviciosimp.ServiciosRemotosClinicaFactory;
+
+public class EnviadorDeOrdenesAlLaboratorio extends ServiciosAplicacionClinica {
+
+	public void enviarOrdenesAutorizadas() {
+		Collection<OrdenMedica> todasLasOrdenes = RepositoriosProvider
+				.getInstancia().getRepositorioOrdenes().getAll();
+		CollectionUtils.filter(todasLasOrdenes, new Predicate() {
+
+			public boolean evaluate(Object arg0) {
+				OrdenMedica ordenMedica = (OrdenMedica) arg0;
+				return ((!ordenMedica.isEnviadaALab()) && (ordenMedica
+						.getEstado().getDescripcion()
+						.equals(OrdenMedicaEstado.AUTORIZADA)));
+			}
+
+		});
+		if (!todasLasOrdenes.isEmpty())
+			ServiciosRemotosClinicaFactory.getInstance()
+					.getServicioRemotoOrdenMedica().enviarOrdenLaboratorio(
+							todasLasOrdenes, "lab1");
+
+	}
+}
