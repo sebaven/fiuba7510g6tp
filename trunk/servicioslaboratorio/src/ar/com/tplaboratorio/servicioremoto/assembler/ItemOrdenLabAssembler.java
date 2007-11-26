77fae5cb0c8617cf91ea7d1797976aacf6518e05
carbonecar <carbonecar@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 00:05:40 +0000

    servicios del lado del laboratorio

---
+++ trunk/servicioslaboratorio/src/ar/com/tplaboratorio/servicioremoto/assembler/ItemOrdenLabAssembler.java
@@ -0,0 +1,17 @@
+package ar.com.tplaboratorio.servicioremoto.assembler;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import ar.com.tplaboratorio.negocio.ItemOrdenMedica;
+import ar.com.tplaboratorio.servicios.orden.dto.ItemOrdenMedicaDTO;
+
+public class ItemOrdenLabAssembler {
+
+	public ItemOrdenMedica ensamblarItem(ItemOrdenMedicaDTO itemOrdenMedicaDTO) {
+		
+		ItemOrdenMedica itemOrdenMedica;
+		//TODO completar
+		throw new NotImplementedException();
+		
+	}
+
+}
--- trunk/servicioslaboratorio/src/ar/com/tplaboratorio/servicios/ServiciosRemotosLParaClinica.java
+++ trunk/servicioslaboratorio/src/ar/com/tplaboratorio/servicios/ServiciosRemotosLParaClinica.java
@@ -1,10 +1,23 @@
 package ar.com.tplaboratorio.servicios;
 
-import ar.com.tplaboratorio.servicios.orden.dto.OrdenesMedicasDTO;
+import ar.com.tpclinica.persistencia.RepositoriosProvider;
+import ar.com.tplaboratorio.negocio.OrdenMedicaLab;
+import ar.com.tplaboratorio.servicioremoto.OrdenAssembler;
+import ar.com.tplaboratorio.servicios.orden.dto.OrdeneMedicaDTO;
+
 
 public class ServiciosRemotosLParaClinica {
 
-	public void recibirOrdenesMedicas(OrdenesMedicasDTO[] ordenes){
-		
+	/**
+	 * Toma todas las ordenes medicas y las persite en el sistema
+	 * @param ordenes
+	 */
+	public void recibirOrdenesMedicas(OrdeneMedicaDTO[] ordenes){
+		OrdenAssembler ordenAssembler=new OrdenAssembler();
+		for (int i = 0; i < ordenes.length; i++) {
+			OrdeneMedicaDTO ordenMedicaDTO=ordenes[i];
+			OrdenMedicaLab ordenMedicaLab=ordenAssembler.createOrdenMedicaLab(ordenMedicaDTO);
+			RepositoriosProvider.getInstancia().getRepositorioOrdenes().add(ordenMedicaLab);
+		}
 	}
 }
