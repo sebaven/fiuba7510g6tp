bc502f91c0ac933acb4ff7495aca62aba7cbdbbc
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 06:54:45 +0000

    Constructores agregados

--- trunk/dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/OrdenMedicaItem.java
+++ trunk/dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/OrdenMedicaItem.java
@@ -2,26 +2,38 @@ package ar.com.tpclinica.negocio;
 
 public class OrdenMedicaItem {
 	private String descripcion;
+
 	private Resultado resultado;
+
 	private Prestacion prestacion;
+
+	public OrdenMedicaItem(Prestacion prestacion2) {
+		this.setPrestacion(prestacion2);
+		this.setDescripcion(prestacion2.getDescripcion());
+	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public Resultado getResultado() {
 		return resultado;
 	}
+
 	public void setResultado(Resultado resultado) {
 		this.resultado = resultado;
 	}
+
 	public Prestacion getPrestacion() {
 		return prestacion;
 	}
+
 	public void setPrestacion(Prestacion prestacion) {
 		this.prestacion = prestacion;
 	}
-	
 
 }
