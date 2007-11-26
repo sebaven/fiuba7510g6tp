cc356d76448b73a0bca0f806926b4e21a055ebcc
yoelseba <yoelseba@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 03:42:21 +0000

    implementé getValor()

--- dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/reglas/MesesEnPlan.java
+++ dominioclinica/src_modeloclinica/ar/com/tpclinica/negocio/reglas/MesesEnPlan.java
@@ -1,12 +1,16 @@
 package ar.com.tpclinica.negocio.reglas;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import ar.com.tpclinica.negocio.OrdenMedica;
 
 public class MesesEnPlan extends Operando {
 	public int getValor(OrdenMedica om)
 	{
-		//TODO!!!
-		return 0;
+		//TODO!!! // cant de meses q tiene el usr en el plan
+		return getDifEnMeses(om.getPaciente().getFechaInicioPlan(), new Date());
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -15,4 +19,51 @@ public class MesesEnPlan extends Operando {
 			return true;
 		else return false;
 	}
+	
+	
+    public static int getDifEnMeses( java.util.Date pFrom, java.util.Date pTo )
+    {
+        int lMultiplier = 1;
+        if ( pFrom.after( pTo ) );
+        {
+            lMultiplier = -1;
+            java.util.Date pTemp = pTo;
+            pTo = pFrom;
+            pFrom = pTemp;
+        }
+ 
+        Calendar lFrom = new GregorianCalendar();
+        lFrom.setTime( pFrom );
+        Calendar lTo = new GregorianCalendar();
+        lTo.setTime( pTo );
+ 
+        int lFromYear = lFrom.get( Calendar.YEAR );
+        int lFromMonth = lFrom.get( Calendar.MONTH );
+        int lFromDay = lFrom.get( Calendar.DAY_OF_MONTH );
+ 
+        int lToYear = lTo.get( Calendar.YEAR );
+        int lToMonth = lTo.get( Calendar.MONTH );
+        int lToDay = lTo.get( Calendar.DAY_OF_MONTH );
+ 
+        int lYearDiff = lToYear - lFromYear;
+        int lMonthDiff = lToMonth - lFromMonth;
+        int lDayDiff = lToDay - lFromDay;
+ 
+        if ( lDayDiff < 0 )
+        {
+            lMonthDiff--;
+            Calendar lTemp = new GregorianCalendar();
+            lTemp.setTime( pTo );
+            lTemp.add( lTemp.MONTH, -1 );
+            lDayDiff = lTemp.getActualMaximum( lTemp.DAY_OF_MONTH ) + lDayDiff;
+        }
+ 
+        if ( lMonthDiff < 0 )
+        {
+            lYearDiff--;
+            lMonthDiff = 12 + lMonthDiff;
+        }
+ 
+        return lMultiplier * ( lYearDiff * 12 + lMonthDiff );
+    }
 }
