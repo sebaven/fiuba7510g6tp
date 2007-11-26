3cca6001a5d9df57a66e98d86c3fd95408d4f031
hgastaud <hgastaud@2f48d00b-b83b-0410-a6a7-f381d0ff53af>
2007-11-26 06:57:32 +0000

    First Load.

---
+++ dominioclinica/test_modeloclinica/ar/com/tpclinica/negocio/reglas/PrestacionesPorPeriodoConHistorialJUnitTC.java
@@ -0,0 +1,11 @@
+package ar.com.tpclinica.negocio.reglas;
+
+import junit.framework.TestCase;
+
+public class PrestacionesPorPeriodoConHistorialJUnitTC extends TestCase {
+
+	
+	public void testGetValor() throws Exception {
+		
+	}
+}
